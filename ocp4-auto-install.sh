7a156d1b946ecdf8c42b8636e9568efffb14f0a2
rcarrata <rcarrata@redhat.com>
2020-03-11 20:19:13 +0100

    added post install oauth

--- ocp4-auto-install.sh
+++ ocp4-auto-install.sh
@@ -4,11 +4,10 @@ podman build -t ocp4-auto-install:latest images/
 
 podman rm -fi ocp4-auto-install
 
-podman run --name ocp4-auto-install --tty --workdir /opt/ocp4-auto-install --privileged \
---volume=/sys/fs/cgroup:/sys/fs/cgroup:ro --volume=`pwd`:/opt/ocp4-auto-install ocp4-auto-install:latest \
+podman run --name ocp4-auto-install --tty --workdir /opt/ocp4-auto-install --privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro --volume=`pwd`:/opt/ocp4-auto-install ocp4-auto-install:latest \
 ansible-playbook -i ,localhost playbooks/ocp4-auto-install.yml --vault-password-file .vault-password-file
 
-# For testing purposes run the container in privileged mode and test the ansible-playbooks inside
+# For testing purposes run the container and test the ansible-playbooks inside
 #podman run --name ocp4-auto-install -ti --workdir /opt/ocp4-auto-install --privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro --volume=`pwd`:/opt/ocp4-auto-install ocp4-auto-install:latest
 
 

2f2c68ecfa98ee4f716e6eb9bc327d7b7e39ae52
rcarrata <rcarrata@redhat.com>
2020-06-11 13:18:19 +0200

    added reestructure

--- ocp4-auto-install.sh
+++ ocp4-auto-install.sh
@@ -4,7 +4,7 @@ podman build -t ocp4-auto-install:latest images/
 
 podman rm -fi ocp4-auto-install
 
-podman run --name ocp4-auto-install --tty --workdir /opt/ocp4-auto-install --privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro --volume=`pwd`:/opt/ocp4-auto-install ocp4-auto-install:latest ansible-playbook -i ,localhost deploy.yml --vault-password-file .vault-password-file
+podman run --name ocp4-auto-install --tty --workdir /opt/ocp4-auto-install --privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro --volume=`pwd`:/opt/ocp4-auto-install ocp4-auto-install:latest ansible-playbook -i ,localhost deploy_all.yml
 
 # For testing purposes run the container and test the ansible-playbooks inside
 #podman run --name ocp4-auto-install -ti --workdir /opt/ocp4-auto-install --privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro --volume=`pwd`:/opt/ocp4-auto-install ocp4-auto-install:latest
